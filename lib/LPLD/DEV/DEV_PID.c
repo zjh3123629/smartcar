32bda74746fcb630263a7c54c5801c689a6809ac
zhouzuoyu <zhouzuoyu@huaqin.com>
2014-08-13 18:29:01 +0800

    增加位置式pid代码

--- lib/LPLD/DEV/DEV_PID.c
+++ lib/LPLD/DEV/DEV_PID.c
@@ -17,6 +17,7 @@ int pid_set_parameters(pid_inittypedef * pid, uint16 setpoint, float setkp, floa
 }
 
 // return pid controller output
+// origin
 int16 pid_increment(pid_inittypedef * pid, uint16 feedback)
 {
 	int16 delta_uk = 0;
@@ -39,3 +40,62 @@ int16 pid_increment(pid_inittypedef * pid, uint16 feedback)
 
 	return pid->uk;
 }
+
+// origin
+uint32 pid_position(pid_inittypedef* pid, uint32 feedback)
+{
+	pid->ek = pid->point - feedback;
+
+	// u(t) = kp * e(t) + ki * [e(1) + e(2) + ....+ e(t)] + kd * [e(t) - e(t-1)]
+	pid->integral += pid->ek;
+	pid->uk = pid->kp*pid->ek + pid->ki*pid->integral + pid->kd*(pid->ek-*pid->ek_1);
+
+	// limit
+	if (pid->uk < 0)
+		pid->uk = 0;
+	else if (pid->uk > PID_CONTROLLER_MAX_OUTPUT)
+		pid->uk = PID_CONTROLLER_MAX_OUTPUT;
+
+	//pid->ek_2 = pid->ek_1;
+	pid->ek_1 = pid->ek;
+
+	return pid->uk;
+}
+
+float pid_calc(struct _pid *pid)
+{
+	int err;
+	float pterm, dterm, result, ferror;
+
+	err = (pid->sp) - (pid->pv);
+
+	// deadband
+	if (abs(err) > pid->deadband) {
+		ferror = (float) err;
+
+		pterm = pid->pgain * ferror;
+		// integral separation
+		if (pterm > 100 || pterm < -100)
+			pid->integral = 0.0;
+		else {
+			pid->integral += pid->igain * ferror;
+
+			// limit integral item
+			if (pid->integral > 100.0)
+				pid->integral = 100.0;
+			else if (pid->integral < 0.0)
+				pid->integral = 0.0;
+		}
+
+		dterm = ((float)(err - pid->last_error)) * pid->dgain;
+		result = pterm + pid->integral + dterm;
+	}
+	else
+		result = pid->integral;
+
+	pid->last_error = err;
+
+	return (result);
+}
+
+
